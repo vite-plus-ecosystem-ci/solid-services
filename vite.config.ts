8d0f9d594dfcf267aafb0dc901f881f61092f87b
MK <fengmk2@gmail.com>
2026-09-17 01:54:56 +0800

    test: verify Vitest 5 preview
    vite-plus 0.0.0-commit.134033efc595ae5efab9df5c177a9ae20904a5d9

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  test: { clearMocks: false },
   fmt: { sortImports: {}, sortPackageJson: {} },
   lint: { options: { typeAware: true, typeCheck: true } },
-  pack: {
-    sourcemap: true,
-  },
+  pack: { deps: { resolveDepSubpath: true }, sourcemap: true },
   resolve: {
     conditions: ["browser"],
   },
